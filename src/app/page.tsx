3662ee312ba91c3fc4dd73e5eb03cc64bc345d8b
MrMiamm <cherwill@laposte.net>
2024-11-20 23:44:51 +0100

    Add project showcase section with details and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,6 +100,43 @@ export default function Portfolio() {
             </div>
         </section>
 
+        <div id="gotoProjets" className="goto"></div>
+        <section className={styles.projets}>
+            <h1 id="projets">Projets</h1>
+            <div>
+                <div className={styles.projet}>
+                    <h2>Puissance 4</h2>
+                    <Image src="/img/puissance4.webp" alt="Puissance4" loading="lazy" width={300} height={300} />
+                    <p>Puissance 4 réalisé en C, s'exécutant dans le terminal.</p>
+                    <a className="see-button" href="projects/puissance4.html"><div>En savoir plus</div></a>
+                </div>
+                <div className={styles.projet}>
+                    <h2>Convertisseur de PDF</h2>
+                    <Image src="/img/convertisseur_pdf.webp" alt="Créateur de PDF" loading="lazy" width={300} height={300} />
+                    <p>Script permettant de générer des fichiers PDF à partir de documents textes</p>
+                    <a className="see-button" href="projects/pdf_creator.html"><div>En savoir plus</div></a>
+                </div>
+                <div className={styles.projet}>
+                    <h2>Création d'une BDD</h2>
+                    <Image src="/img/creation_bdd.webp" alt="Création d'une BDD" loading="lazy" width={300} height={300} />
+                    <p>Base de données réalisée avec Postgresql.</p>
+                    <a className="see-button" href="projects/bdd_creation.html"><div>En savoir plus</div></a>
+                </div>
+                <div className={styles.projet}>
+                    <h2>Sport KOH</h2>
+                    <Image src="/img/sport_koh.webp" alt="Sport KOH" loading="lazy" width={300} height={300} />
+                    <p>Site internet vendant des voyages sportifs.</p>
+                    <a className="see-button" href="projects/sport_koh.html"><div>En savoir plus</div></a>
+                </div>
+            </div>
+            <svg className={styles.wave_projects_top} data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+            </svg>
+            <svg className="wave_bottom" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+            </svg>
+        </section>
+
         <Footer />
         </>
     );
