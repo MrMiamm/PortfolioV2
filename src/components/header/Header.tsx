6b4365169849d5c3e900e530d4b5adbabb8e1775
MrMiamm <cherwill@laposte.net>
2024-11-21 16:40:16 +0100

    Enhance header functionality with scroll effect and burger menu toggle

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,44 @@
 "use client";
 
-import Link from "next/link";
-import NavLinks from "./NavLinks";
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+import NavLinks from "./NavLinks";
 
 export default function Header() {
+
+    useEffect(() => {
+
+        const header = document.querySelector("header");
+        function addHeaderScrolledClass() {
+            if (header) {
+                window.scrollY > 0 ? header.classList.add("scrolled") : header.classList.remove("scrolled");
+            }
+        }
+        
+        window.addEventListener("scroll", () => addHeaderScrolledClass());
+        addHeaderScrolledClass();
+
+        return () => {
+            window.removeEventListener("scroll", () => addHeaderScrolledClass());
+        };
+    }, []);
+
+    useEffect(() => {
+        // Handle closing burger menu when link is clicked
+        const toggle = document.getElementById("toggle");
+        const navLinks = document.querySelectorAll(".pagelink");
+        
+        navLinks.forEach((link) => {
+            link.addEventListener("click", () => {
+                if (toggle && toggle instanceof HTMLInputElement) {
+                    toggle.checked = false;
+                }
+            });
+        });
+
+    }, []);
+
     return (
         <>
         <input type="checkbox" id="toggle" />
--- src/lib/lenis.js
+++ src/lib/lenis.js
@@ -5,9 +5,7 @@ let lenis;
 export function initLenis() {
     if (!lenis) {
         lenis = new Lenis({
-            duration: 1.1,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            wheelMultiplier: 0.8
+            smooth: 0.1,
         });
 
         // Boucle d'animation
@@ -22,7 +20,7 @@ export function initLenis() {
 
 export function getLenis() {
     if (!lenis) {
-        throw new Error('Lenis n\'a pas été initialisé. Appelez initLenis() d\'abord.');
+        lenis = initLenis();
     }
     return lenis;
 }
